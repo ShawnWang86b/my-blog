804366f1d939dd13dbab39f1397f04f22bee9233
shuzheng wang <shawn.wang86b@gmail.com>
2022-12-05 00:59:30 +1100

    feat: inital commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Provider } from "react-redux";
-import { store } from "../store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <ChakraProvider>
-        <Component {...pageProps} />;
-      </ChakraProvider>
-    </Provider>
+        <Component {...pageProps} />
   );
 }
